7ae4e300f137b0c992673a39db12aebf8e8a1457
Magdalena Buzikiewicz <buzu@op.pl>
2020-11-11 18:57:37 +0100

    Basic components structure.

---
+++ src/components/Post/Post.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Posts() {
+
+  return (
+    <>
+      <Post />
+    </>
+  );
+}
+
+function Post() {
+
+  return (
+    <>
+      infinite scroll
+    </>
+  );
+}
+
+export default Posts;
