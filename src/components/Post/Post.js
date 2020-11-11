f2a7bc8f568104c53359e9ce992cb9a8608cea65
Magdalena Buzikiewicz <buzu@op.pl>
2020-11-11 21:05:12 +0100

    Fetching data from local json server.

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Spinner from "../Spinner";
 
-function Posts() {
 
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const API = "http://localhost:3004/posts";
+
+  useEffect(() => {
+    fetch(`${API}`)
+    .then(response => response.json())
+    .then(response => setPosts(response))
+    .then(console.log("posts; ", posts))
+    .catch(error => {
+      console.log("There was an error with request: ", error);
+    });
+  }, [posts]);
+
+
+  if (posts === []) {
+    return <Spinner />
+  }
   return (
-    <>
-      <Post />
-    </>
-  );
-}
-
-function Post() {
-
-  return (
-    <>
-      infinite scroll
-    </>
+    <div className="container">
+      <ul>
+        {posts.map(post => {
+          return (
+            <li className="post_element" key={post.date}>
+              <img className="picture" src={post.thumb} alt={post.title} />
+              <h2 className="event_name">{post.date}  {post.title}</h2>
+              <p className="peak_hour"> {post.excerpt}</p>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   );
 }
 
